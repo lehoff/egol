17cba4b82b9b483725312f616a9539230ece3b7e
Torben Hoffmann <torben.lehoff@gmail.com>
2014-11-21 00:12:12 +0100

    Changed egol_cell to spawn a process to collect from its neighbours

--- src/egol_cell.erl
+++ src/egol_cell.erl
@@ -13,12 +13,15 @@
          pause/1,
          step/1]).
          
+-type mode() :: 'run' | 'step'.
 
 -record(state,
         { xy,
           dim,
           content=0 :: 0..1,
           time=0,
+          collector :: pid(),
+          mode=step :: mode(),
           future= [] :: [{pid(), pos_integer()}],
           history=[],
           neighbours}).
@@ -74,86 +77,143 @@ cmd_sync(To, Cmd) ->
 %% states
 init(#state{xy=XY}=State) ->
   reg(XY),
-  idle(State).
+  loop(State).
 
 
-idle(State) ->
+loop(#state{xy=XY, time=T, content=Content, collector=Collector,
+            future=Future, history=History}=State) ->
   receive 
     {set, NewContent} ->
-      idle(State#state{content=NewContent});
+      loop(State#state{content=NewContent});
     {From, {get, Time}} ->
       case content_at(Time, State) of
         future ->
-          idle(State#state{future=[{From, Time} | State#state.future]});
+          loop(State#state{future=[{From, Time} | State#state.future]});
         C ->
           From ! {cell_content, C},              
-          idle(State)
+          loop(State)
       end;
     {From, history} ->
       #state{history=History} = State,
       From ! History,
-      idle(State);
+      loop(State);
     {From, time} ->
       From ! State#state.time,
-      idle(State);
+      loop(State);
     run ->
-      running(State);
-    step ->
-      NewState=run_step(State),
-      idle(NewState);
-    pause ->
-      idle(State)    
-  end.
-
-running(#state{time=T, neighbours=Neighbours}=State) ->
-  query_neighbours(T, Neighbours),
-  NewState = collecting(State, 0, neighbours_at(T, Neighbours)),
-  receive
-    pause ->
-      idle(NewState);
-    step ->
-      NextState = collecting(NewState, 0, Neighbours),
-      idle(NextState)
-  after
-    0 ->
-      running(NewState)
-  end.
-
-run_step(#state{time=T, neighbours=Neighbours}=State) ->
-  query_neighbours(T, Neighbours),
-  collecting(State, 0, neighbours_at(T, Neighbours)).
-
-collecting(#state{xy=XY, content=Content, time=T, history=History, future=Future}=State, NeighbourCount, []) ->
-  NextContent = next_content(Content, NeighbourCount),
-  NewFuture = process_future(XY, T+1, NextContent, Future),
-  lager:info("Cell ~p changing to ~p for time ~p", [XY, NextContent, T+1]),
-  State#state{content=NextContent,
-              time=T+1,
-              history=[{T, Content}|History],
-              future=NewFuture};
-collecting(#state{}=State, NeighbourCount, WaitingOn) ->
-  receive
-    {From, {get, Time}} ->
-      case content_at(Time, State) of
-        future ->
-          collecting(State#state{future=[{From, Time}|State#state.future]},
-                     NeighbourCount, WaitingOn);
-        C ->
-          From ! {cell_content, C},
-          collecting(State, NeighbourCount, WaitingOn)
+      case is_collector_running(State) of
+        true ->
+          loop(State#state{mode=run});
+        false ->
+          NextState = start_collector(State),
+          loop(NextState#state{mode=run})
       end;
+    step ->
+      case is_collector_running(State) of
+        true ->
+          loop(State#state{mode=step});
+        false ->
+          NewState = start_collector(State),
+          loop(NewState#state{mode=step})
+      end;
+    pause ->
+      loop(State#state{mode=step});
+    {Collector, {next_content, NextContent}} ->
+      NewFuture = process_future(XY, T+1, NextContent, Future),
+      lager:info("Cell ~p changing to ~p for time ~p", [XY, NextContent, T+1]),
+      NextState = State#state{content=NextContent,
+                              time=T+1,
+                              history=[{T, Content}|History],
+                              future=NewFuture}, 
+      case State#state.mode of
+        step ->
+          loop(NextState);
+        run ->
+          loop(start_collector(NextState))
+      end
+  end.
+
+is_collector_running(#state{collector=Collector}) ->
+    is_pid(Collector) andalso is_process_alive(Collector).
+
+start_collector(#state{time=T, neighbours=Neighbours, content=Content}=State) ->
+  Cell = self(),
+  Collector = spawn( fun () ->
+                         collector_init(T, Neighbours, Cell, Content)
+                     end ),
+  State#state{collector=Collector}.
+
+collector_init(Time, Neighbours, Cell, Content) ->
+  query_neighbours(Time, Neighbours),
+  collector_loop(neighbours_at(Time, Neighbours), 0, Cell, Content).
+
+collector_loop([], NeighbourCount, Cell, Content) ->
+  Cell ! {self(), {next_content, next_content(Content, NeighbourCount)}};
+collector_loop(WaitingOn, NeighbourCount, Cell, Content) ->
+  receive
     {cell_content, {{{_,_},_}=XYatT, NeighbourContent}} ->
       case lists:member(XYatT, WaitingOn) of
         true ->
-          collecting(State, NeighbourCount + NeighbourContent, lists:delete(XYatT, WaitingOn));
+          collector_loop(lists:delete(XYatT, WaitingOn),
+                         NeighbourCount + NeighbourContent,
+                         Cell, Content);
         false %% ignore messages we are not waiting for
               ->
-          collecting(State, NeighbourCount, WaitingOn)
+          collector_loop(WaitingOn, NeighbourCount, Cell, Content)
       end;
-    {set, NewContent} %% fun stuff can happen if you change the state while running...
-                      ->
-      collecting(State#state{content=NewContent}, NeighbourCount, WaitingOn)
+    {set, NewContent} -> %% this could be fun...
+      collector_loop(WaitingOn, NeighbourCount, Cell, NewContent)
   end.
+
+%% running(#state{time=T, neighbours=Neighbours}=State) ->
+%%   query_neighbours(T, Neighbours),
+%%   NewState = collecting(State, 0, neighbours_at(T, Neighbours)),
+%%   receive
+%%     pause ->
+%%       loop(NewState);
+%%     step ->
+%%       NextState = collecting(NewState, 0, Neighbours),
+%%       loop(NextState)
+%%   after
+%%     0 ->
+%%       running(NewState)
+%%   end.
+
+%% run_step(#state{time=T, neighbours=Neighbours}=State) ->
+%%   query_neighbours(T, Neighbours),
+%%   collecting(State, 0, neighbours_at(T, Neighbours)).
+
+%% collecting(#state{xy=XY, content=Content, time=T, history=History, future=Future}=State, NeighbourCount, []) ->
+%%   NextContent = next_content(Content, NeighbourCount),
+%%   NewFuture = process_future(XY, T+1, NextContent, Future),
+%%   lager:info("Cell ~p changing to ~p for time ~p", [XY, NextContent, T+1]),
+%%   State#state{content=NextContent,
+%%               time=T+1,
+%%               history=[{T, Content}|History],
+%%               future=NewFuture};
+%% collecting(#state{}=State, NeighbourCount, WaitingOn) ->
+%%   receive
+%%     {From, {get, Time}} ->
+%%       case content_at(Time, State) of
+%%         future ->
+%%           collecting(State#state{future=[{From, Time}|State#state.future]},
+%%                      NeighbourCount, WaitingOn);
+%%         C ->
+%%           From ! {cell_content, C},
+%%           collecting(State, NeighbourCount, WaitingOn)
+%%       end;
+%%     {cell_content, {{{_,_},_}=XYatT, NeighbourContent}} ->
+%%       case lists:member(XYatT, WaitingOn) of
+%%         true ->
+%%           collecting(State, NeighbourCount + NeighbourContent, lists:delete(XYatT, WaitingOn));
+%%         false %% ignore messages we are not waiting for
+%%               ->
+%%           collecting(State, NeighbourCount, WaitingOn)
+%%       end;
+%%     {set, NewContent} %% fun stuff can happen if you change the state while running...
+%%                       ->
+%%       collecting(State#state{content=NewContent}, NeighbourCount, WaitingOn)
+%%   end.
 
 process_future(XY, Time, Content, Future) ->
   {Ready, NewFuture} = lists:partition( fun({_Pid,T}) ->
